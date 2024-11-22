7026a68e52af0ca1f73d95505834cb1a5fc00da2
Caio Castelhano <castelhanoc@gmail.com>
2024-11-22 12:39:04 -0300

    back-end feito

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,24 +1,26 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
 
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
-
+  
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
 
-    redirect_to lists_path(@bookmark.list)
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
-  # rotas de lists passo a passo
-  # get "lists", to: "lists#index"
-  # get "lists/new", to: "lists#new", as: "new_list"
-  # post "lists", to: "lists#create"
-  # get "lists/:id", to: "lists#show" 
-
-  # rotas de listas transformadas em resources
-  # mostrar todas as listas
-  # criar uma nova lista
-  # mostrar uma lista através do id
-  resources :lists, only: [:index, :new, :create, :show] do
+  root to: "lists#index"
+  resources :lists, only: [:index, :new, :create, :show, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
 
-  # rotas de bookmarks passo a passo
-  resources :bookmarks, only: [:destroy]
-  
+  resources :bookmarks, only: :destroy
 end
